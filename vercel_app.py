f404fdea184e205bb97851a1865d1a0e20d2faee
Sukrit Tripathi <sukrit@roadtoivies.com>
2026-05-16 14:11:20 +0530

    build: migrate to native api runtime architecture

--- vercel_app.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import streamlit.web.cli as stcli
-
-if __name__ == "__main__":
-    sys.argv = ["streamlit", "run", "app.py", "--server.port", "8501", "--server.address", "0.0.0.0"]
-    sys.exit(stcli.main())
